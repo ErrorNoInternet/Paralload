031df7439200ba5d710f9087bc99687caf69951e
ErrorNoInternet <errornointernet@envs.net>
2023-09-04 15:28:51 +0800

    Remove 32-bit

--- build.sh
+++ build.sh
@@ -6,8 +6,5 @@ mkdir bin
 echo "Compiling Linux 64-bit..."
 go build -ldflags "-s -w" -o bin/paralload_linux-amd64 -v
 
-echo "Compiling Linux 32-bit..."
-go build -ldflags "-s -w" -o bin/paralload_linux-386 -v
-
 echo "Compiling Windows 64-bit..."
 CC="zig cc -target x86_64-windows-gnu" CXX="zig c++ -target x86_64-windows-gnu" GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -ldflags "-s -w" -o bin/paralload_windows-amd64.exe -v
